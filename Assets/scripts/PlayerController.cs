724886a0d6042989261e68f9918e1aff24b24acf
abdelrahmanbeedo <abdelrahman.mahmoud23b@eslsca.edu.eg>
2025-11-03 23:49:58 +0200

    polish

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
     public TMP_Text scoreText;
+    public Transform model;
 
     private Rigidbody rb;
     private Vector2 inputMovement;
@@ -26,8 +27,19 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
 {
-Vector3 movement = transform.right * inputMovement.x + transform.forward * inputMovement.y;
-rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+  Vector3 movement = transform.right * inputMovement.x + transform.forward * inputMovement.y;
+  rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
 
+if (movement.magnitude > 0.1f)
+{
+    // Figure out which way the player is moving
+    float angle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+
+    // Make only the model rotate visually
+    Quaternion currentRotation = model.localRotation;
+    Quaternion targetRotation = Quaternion.Euler(0f, angle, 0f);
+    model.localRotation = Quaternion.Slerp(currentRotation, targetRotation, 10f * Time.deltaTime);
+
+}
 }
 }
